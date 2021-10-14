44cb4831ee6f3a919d273ceef33897b729af06f5
izenynn <me@izenynn.com>
2021-10-14 22:49:38 +0200

    Optimizations

--- src/sort_big.c
+++ src/sort_big.c
@@ -12,13 +12,43 @@
 
 #include <push_swap.h>
 
+static int	get_push_chunk_dir(t_global *tab, int chunk_sz, int chunk_n)
+{
+	int	cnt;
+	int	pos_first;
+	int	pos_last;
+
+	cnt = 0;
+	pos_first = -1;
+	pos_last = 0;
+	tab->a = tab->head_a;
+	while (tab->a)
+	{
+		if (*(int *)tab->a->data < chunk_sz * chunk_n)
+		{
+			if (pos_first < 0)
+				pos_first = cnt;
+			pos_last = cnt;
+		}
+		cnt++;
+		tab->a = tab->a->next;
+	}
+	if (pos_last < tab->a_sz - pos_first)
+		return (1);
+	return (0);
+	// 8, 3, sz: 20
+	// 8, 20 - 3 = 17
+}
+
 static void	push_in_chunks(t_global *tab, int chunk_sz)
 {
 	int	chunk_n;
 	int	cnt;
+	int	dir;
 
 	chunk_n = 1;
 	cnt = 0;
+	dir = get_push_chunk_dir(tab, chunk_sz, chunk_n);
 	while (tab->a_sz)
 	{
 		if (*(int *)tab->head_a->data < (chunk_sz * chunk_n))
@@ -29,10 +59,15 @@ static void	push_in_chunks(t_global *tab, int chunk_sz)
 				rb(tab);
 			cnt++;
 		}
+		else if (dir)
+			rra(tab);
 		else
 			ra(tab);
 		if (cnt == chunk_sz * chunk_n)
+		{
 			chunk_n++;
+			dir = get_push_chunk_dir(tab, chunk_sz, chunk_n);
+		}
 	}
 }
 
